bcf4bf0f1fe41ab26e600d5da21730994de0c35d
Aya <squallihoussainiaya@gmail.com>
2025-04-07 11:35:40 +0100

    Add needed components for landing page

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+   <nav-bar></nav-bar>
+   <search-bar></search-bar>
+   <disc-item></disc-item>
+</template>
+
+
+<script>
+import NavBar from '@/components/NavBar';
+
+
+</script>
+<style scoped>
+
+</style>
