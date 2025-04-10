240b258432f4437952d99c060b4d0dc4359641d2
SniAssia <assia.snissi@um6p.ma>
2025-04-10 10:34:25 +0000

    discussions

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+import DiscussionDetail from '@/components/DiscussionDetail.vue'
+import DiscussionList from '@/components/DiscussionList.vue'
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Discussionlist',
+    component: DiscussionList,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/detail/:id', // :id est un paramètre dynamique
+    name: 'DiscussionDetail',
+    component: DiscussionDetail,
+  },
 ]
 
 const router = createRouter({
